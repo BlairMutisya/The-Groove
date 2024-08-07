523b988f47fab42a203e7496ca9de4659b6aa46e
Robert Gathanua <robertgathanua@gamail.com>
2024-08-07 14:35:21 +0300

    Add the necessary React hooks in the Footer component

---
+++ front-end/src/components/client/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { PiCopyright } from "react-icons/pi";
+import { LiaWhatsapp } from "react-icons/lia";
+import { PiFigmaLogoLight } from "react-icons/pi";
+import { PiYoutubeLogoLight } from "react-icons/pi";
+import { CiFacebook } from "react-icons/ci";
+
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
