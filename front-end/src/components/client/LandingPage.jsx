da5ee09db32489212050c9be9046bade725ca6fd
Robert Gathanua <robertgathanua@gamail.com>
2024-08-07 12:45:26 +0300

    Implement data fetching and state management for products to ensure product data is fetched and managed efficiently

--- front-end/src/components/client/LandingPage.jsx
+++ front-end/src/components/client/LandingPage.jsx
@@ -3,6 +3,32 @@ import React from 'react'
 const apiUrl = 'https://fakestoreapi.com/products';
 
 const LandingPage = () => {
+    const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return (
     <div className='landing page'>LandingPage</div>
   )
