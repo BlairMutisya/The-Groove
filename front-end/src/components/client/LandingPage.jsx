87cbba6ecf92e7cd36dbc60b8ed75373f30c36fd
Robert Gathanua <robertgathanua@gamail.com>
2024-08-07 14:15:38 +0300

    Add layout and design of an example of featured spaces

--- front-end/src/components/client/LandingPage.jsx
+++ front-end/src/components/client/LandingPage.jsx
@@ -73,6 +73,45 @@ const LandingPage = () => {
                         <img className='rounded-lg w-[300px] h-[500px] object-cover mt-24' src='https://img.freepik.com/premium-photo/living-room-with-couch-coffee-table-tv-wall_517312-27396.jpg?uid=R157253250&semt=ais_hybrid' />
                     </div>
                 </div>
+                <div className=''>
+          <div>
+            <h1 className='justify-center items-center flex text-5xl font-bold'>FEATURED SPACES</h1>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4">
+
+            {products.slice(0, 4).map((product) => (
+              <div className='border-slate-950 border-b-2'>
+                <div key={product.id} className="flex bg-[#fff3e5] p-4 mb-8 ">
+                  <div className="w-[250px]">
+                    <img className="rounded-lg w-full h-48 object-cover" src={product.image} alt={product.title} />
+                  </div>
+                  <div className="ml-5 flex-1">
+                    <p className="text-lg font-semibold mb-2">{product.title}</p>
+                    <p className="text-gray-600 mb-4">Location: {product.category}</p>
+                    <div className="flex items-center">
+                      {/* Display product rating as stars */}
+                      {[...Array(5)].map((_, index) => (
+                        <svg
+                          key={index}
+                          className={`w-4 h-4 ${index < Math.round(product.rating.rate) ? 'text-yellow-300' : 'text-gray-300'} ms-1`}
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 22 20"
+                        >
+                          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                      ))}
+                    </div>
+                    
+                  </div>
+                </div>
+              </div>
+
+            ))}
+
+          </div>
+        </div>
             </div>
         </div>
     )
