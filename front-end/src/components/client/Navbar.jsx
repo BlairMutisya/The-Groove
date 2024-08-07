da85e6095068bcc410d37a372757fecdd08ec0f4
Robert Gathanua <robertgathanua@gamail.com>
2024-08-07 14:55:12 +0300

    Add imports for React icons and routing to support icons and routing functionality in the component

---
+++ front-end/src/components/client/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FaBars } from "react-icons/fa";
+import { VscAccount } from "react-icons/vsc";
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
