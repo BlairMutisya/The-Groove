36f12ad1797b25a3beeb1041cc70022b5afdcf63
Robert Gathanua <robertgathanua@gamail.com>
2024-08-07 14:30:52 +0300

    Add the styling and layout of the user review cards section

--- front-end/src/components/client/LandingPage.jsx
+++ front-end/src/components/client/LandingPage.jsx
@@ -74,44 +74,87 @@ const LandingPage = () => {
                     </div>
                 </div>
                 <div className=''>
-          <div>
-            <h1 className='justify-center items-center flex text-5xl font-bold'>FEATURED SPACES</h1>
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4">
-
-            {products.slice(0, 4).map((product) => (
-              <div className='border-slate-950 border-b-2'>
-                <div key={product.id} className="flex bg-[#fff3e5] p-4 mb-8 ">
-                  <div className="w-[250px]">
-                    <img className="rounded-lg w-full h-48 object-cover" src={product.image} alt={product.title} />
-                  </div>
-                  <div className="ml-5 flex-1">
-                    <p className="text-lg font-semibold mb-2">{product.title}</p>
-                    <p className="text-gray-600 mb-4">Location: {product.category}</p>
-                    <div className="flex items-center">
-                      {/* Display product rating as stars */}
-                      {[...Array(5)].map((_, index) => (
-                        <svg
-                          key={index}
-                          className={`w-4 h-4 ${index < Math.round(product.rating.rate) ? 'text-yellow-300' : 'text-gray-300'} ms-1`}
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="currentColor"
-                          viewBox="0 0 22 20"
-                        >
-                          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                      ))}
+                    <div>
+                        <h1 className='justify-center items-center flex text-5xl font-bold'>FEATURED SPACES</h1>
                     </div>
-                    
-                  </div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4">
+
+                        {products.slice(0, 4).map((product) => (
+                            <div className='border-slate-950 border-b-2'>
+                                <div key={product.id} className="flex bg-[#fff3e5] p-4 mb-8 ">
+                                    <div className="w-[250px]">
+                                        <img className="rounded-lg w-full h-48 object-cover" src={product.image} alt={product.title} />
+                                    </div>
+                                    <div className="ml-5 flex-1">
+                                        <p className="text-lg font-semibold mb-2">{product.title}</p>
+                                        <p className="text-gray-600 mb-4">Location: {product.category}</p>
+                                        <div className="flex items-center">
+                                            {/* Display product rating as stars */}
+                                            {[...Array(5)].map((_, index) => (
+                                                <svg
+                                                    key={index}
+                                                    className={`w-4 h-4 ${index < Math.round(product.rating.rate) ? 'text-yellow-300' : 'text-gray-300'} ms-1`}
+                                                    aria-hidden="true"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="currentColor"
+                                                    viewBox="0 0 22 20"
+                                                >
+                                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                                </svg>
+                                            ))}
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                        ))}
+
+                    </div>
                 </div>
-              </div>
+                <div className='flex  mt-11 space-y-8'>
 
-            ))}
+                    {/* Single Card Section */}
+                    <div className='w-full  flex justify-center mt-20'>
 
-          </div>
-        </div>
+                        <div className='h-full'>
+                            <div className='w-96 bg-[#FEFEFE] shadow-md hover:scale-105 duration-500 py-2 rounded-lg flex items-center'>
+                                <div class="flex-shrink-0 ml-12 mt-4">
+                                    <img class="w-14 h-14 rounded-full object-cover" src="https://img.freepik.com/free-photo/young-handsome-african-business-man-posing-isolated_231208-5502.jpg?uid=R157253250&semt=sph" alt="Neil image" />
+                                </div>
+                                <div class="flex-1 min-w-0 ms-4">
+                                    <p class="text-sm font-medium text-gray-900 truncate dark:text-black">
+                                        Neil Sims
+                                    </p>
+                                    <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                                        email@windster.com
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    {/* Multiple Cards Section */}
+                    <div className='bg-[#fce8cf] w-full h-auto flex flex-wrap justify-center gap-6 py-8'>
+                        {[1, 2, 3, 4].map((_, index) => (
+                            <div key={index} className='w-80 bg-[#FEFEFE] shadow-md hover:scale-105 duration-300 py-4 px-4 rounded-lg flex items-center'>
+                                <div className='flex-shrink-0 ml-4'>
+                                    <img className='w-12 h-12 rounded-full object-cover' src="https://img.freepik.com/free-photo/young-handsome-african-business-man-posing-isolated_231208-5502.jpg?uid=R157253250&semt=sph" alt="Neil image" />
+                                </div>
+                                <div className='flex-1 min-w-0 ms-4'>
+                                    <p className='text-sm font-medium text-gray-900 truncate'>
+                                        Neil Sims
+                                    </p>
+                                    <p className='text-xs text-gray-500 truncate'>
+                                        email@windster.com
+                                    </p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                </div>
             </div>
         </div>
     )
