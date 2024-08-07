815f27b0bac8b07f4ea22564c48cd4414f85e1af
Robert Gathanua <robertgathanua@gamail.com>
2024-08-07 16:53:53 +0300

    Implement data fetching and state management for products

--- front-end/src/components/client/SpaceList.jsx
+++ front-end/src/components/client/SpaceList.jsx
@@ -6,6 +6,39 @@ import { Link } from 'react-router-dom';
 const apiUrl = 'https://fakestoreapi.com/products';
 
 const SpaceList = () => {
+    // const isBooked = product.isBooked;
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+
+  // Function to handle product selection
+  const handleProductClick = (product) => {
+    setSelectedProduct(product);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return (
     <div>SpaceList</div>
   )
