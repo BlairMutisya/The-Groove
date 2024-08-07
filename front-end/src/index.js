6b1bd1bcb5c217da66b910e50c31b9a6aca651b9
BlairMutisya <tonnyblair09@gmail.com>
2024-08-07 15:40:20 +0300

    Add missing component files

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,15 +1,40 @@
-// App.js or your main component file
 import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import About from "./components/client/About";
+import Agents from "./components/client/Agents";
+import Contact from "./components/client/Contact";
+import SpaceDetails from "./components/client/SpaceDetails";
+import LandingPage from "./components/client/LandingPage";
+import SpaceList from "./components/client/SpaceList";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/spacelist",
+    element: <SpaceList />,
+  },
+  {
+    path: "/agents",
+    element: <Agents />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/listing/details/:id",
+    element: <SpaceDetails />,
+  },
+]);
 
-const App = () => {
-  return (
-    <div>
-      {/* Other components and routes */}
-      <About/>
-    </div>
-  );
-};
-
-export default App;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
