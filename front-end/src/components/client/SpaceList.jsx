13dbefeb17ef0640bfdba2c67e71f4871b6e4af6
Robert Gathanua <robertgathanua@gamail.com>
2024-08-07 16:51:18 +0300

    Update the apiUrl constant to point to the api endpoint for fetching product data

--- front-end/src/components/client/SpaceList.jsx
+++ front-end/src/components/client/SpaceList.jsx
@@ -3,6 +3,8 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import { Link } from 'react-router-dom';
 
+const apiUrl = 'https://fakestoreapi.com/products';
+
 const SpaceList = () => {
   return (
     <div>SpaceList</div>
