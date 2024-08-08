b395a52845c3a2194323f19189b1b96ac96fb7d1
Robert Gathanua <robertgathanua@gamail.com>
2024-08-08 22:19:02 +0300

    Updated the apiUrl to point out to the api endpoint.

--- front-end/src/components/client/SpaceDetails.jsx
+++ front-end/src/components/client/SpaceDetails.jsx
@@ -3,33 +3,33 @@ import { useParams } from 'react-router-dom';
 
 const apiUrl = 'https://fakestoreapi.com/products';
 
-// const SpaceDetails = () => {
-//     const { id } = useParams();
-//     const [product, setProduct] = useState(null);
-//     const [loading, setLoading] = useState(true);
-//     const [error, setError] = useState(null);
+const SpaceDetails = () => {
+    const { id } = useParams();
+    const [product, setProduct] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
   
-//     useEffect(() => {
-//       const fetchProduct = async () => {
-//         try {
-//           const response = await fetch(`${apiUrl}/${id}`);
-//           if (!response.ok) {
-//             throw new Error('Product not found');
-//           }
-//           const data = await response.json();
-//           setProduct(data);
-//         } catch (error) {
-//           setError(error.message);
-//         } finally {
-//           setLoading(false);
-//         }
-//       };
+    useEffect(() => {
+      const fetchProduct = async () => {
+        try {
+          const response = await fetch(`${apiUrl}/${id}`);
+          if (!response.ok) {
+            throw new Error('Product not found');
+          }
+          const data = await response.json();
+          setProduct(data);
+        } catch (error) {
+          setError(error.message);
+        } finally {
+          setLoading(false);
+        }
+      };
   
-//       fetchProduct();
-//     }, [id]);
+      fetchProduct();
+    }, [id]);
   
-//     if (loading) return <div className='flex justify-center text-2xl'>Loading...</div>;
-//     if (error) return <div>Error: {error}</div>;
+    if (loading) return <div className='flex justify-center text-2xl'>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
 
 //   return (
 //     <div className='bg-[#fff3e5]'>
@@ -90,6 +90,6 @@ const apiUrl = 'https://fakestoreapi.com/products';
 //       <Footer />
 //     </div>
 //   )
-// }
+}
 
 export default SpaceDetails
