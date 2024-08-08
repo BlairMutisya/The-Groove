deb5f7f563aec3fb9eef9281b34f586fcfa61924
Robert Gathanua <robertgathanua@gamail.com>
2024-08-08 15:30:05 +0300

    Updated the apiUrl to point out to the api endpoint.

--- front-end/src/components/client/SpaceDetails.jsx
+++ front-end/src/components/client/SpaceDetails.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+const apiUrl = 'https://fakestoreapi.com/products';
+
 const SpaceDetails = () => {
   return (
     <div>SpaceDetails</div>
