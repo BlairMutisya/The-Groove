b4b57aa5806cf2accae746040e150aca01a21f0f
Robert Gathanua <robertgathanua@gamail.com>
2024-08-08 22:12:37 +0300

    Update the apiUrl constant to point to the api endpoint for fetching product data.

--- front-end/src/components/client/SpaceDetails.jsx
+++ front-end/src/components/client/SpaceDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-// const apiUrl = 'https://fakestoreapi.com/products';
+const apiUrl = 'https://fakestoreapi.com/products';
 
 // const SpaceDetails = () => {
 //     const { id } = useParams();
