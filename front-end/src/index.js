89f2448f6dbba1cabe9ae27e1e823f8948442f28
ByteBender404 <122833274+BlairMutisya@users.noreply.github.com>
2024-08-09 12:50:58 +0300

    Update index.js
    Correct the content in the index.js to avoid merge conflicts experienced earlier

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,16 +1,21 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles.css';
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import About from './components/client/About';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./styles.css";
+import About from "./components/client/About";
+import SignUp from "./components/client/SignUp";
+import SignIn from "./components/client/SignIn";
 import Agents from './components/client/Agents';
 import Contact from './components/client/Contact';
 import SpaceDetails from './components/client/SpaceDetails';
 import LandingPage from './components/client/LandingPage';
 import SpaceList from './components/client/SpaceList';
 
+
+const PUBLISHABLE_KEY ="pk_test_dG9sZXJhbnQtZG9nZmlzaC0xMy5jbGVyay5hY2NvdW50cy5kZXYk";
 const router = createBrowserRouter([
+
   {
     path: "/",
     element: <LandingPage />
@@ -34,29 +39,22 @@ const router = createBrowserRouter([
   {
     path:"/listing/details/:id",
     element:<SpaceDetails />
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
   }
-])
+]);
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <RouterProvider router={router} />
-  
+  <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <RouterProvider router={router} />
+    </ClerkProvider>
+  </React.StrictMode>
 );
-=======
-// App.js or your main component file
-import React from "react";
-import About from "./components/client/About";
-
-
-const App = () => {
-  return (
-    <div>
-      {/* Other components and routes */}
-      <About/>
-    </div>
-  );
-};
-
-export default App;
->>>>>>> 87abcd83e719253b027d9f8c2fa1e50d9018aba8
