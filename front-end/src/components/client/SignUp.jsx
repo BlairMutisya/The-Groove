a94c5d8223c5209343b86e7638313c338e409e10
uvyne-rop <ropchiri7@gmail.com>
2024-08-09 15:32:55 +0300

    made the agents page consiting of the agents rental website

---
+++ front-end/src/components/client/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SignUp } from "@clerk/clerk-react";
+
+function SignUpPage() {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
+}
+
+export default SignUpPage;
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./styles.css";
+import About from "./components/client/About";
+import SignUp from "./components/client/SignUp";
+import SignIn from "./components/client/SignIn";
+import Agents from './components/client/Agents';
+import Contact from './components/client/Contact';
+import SpaceDetails from './components/client/SpaceDetails';
+import LandingPage from './components/client/LandingPage';
+import SpaceList from './components/client/SpaceList';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const PUBLISHABLE_KEY ="pk_test_ZmFpci1sZW1taW5nLTc1LmNsZXJrLmFjY291bnRzLmRldiQ";
+const router = createBrowserRouter([
+
+  {
+    path: "/",
+    element: <LandingPage />
+  }, 
+  {
+    path: "/about",
+    element: <About />
+  },
+  {
+    path: "/spacelist",
+    element: <SpaceList />
+  },
+  {
+    path:"/agents",
+    element:<Agents />
+  },
+  {
+    path:"/contact",
+    element:<Contact />
+  },
+  {
+    path:"/listing/details/:id",
+    element:<SpaceDetails />
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
+  }
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <RouterProvider router={router} />
+    </ClerkProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
