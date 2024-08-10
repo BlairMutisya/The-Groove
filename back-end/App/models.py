b1b3c4e8dd05fab0b4dead665bf10802597a2b60
uvyne-rop <ropchiri7@gmail.com>
2024-08-10 22:01:19 +0300

    initialize the serializer with the app secret key

--- back-end/App/models.py
+++ back-end/App/models.py
@@ -21,3 +21,9 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 mail = Mail(app)
 login_manager = LoginManager(app)
+# Initialize the serializer with your app's secret key
+serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+from Backend.run import admin_routes  # Import routes after app is created
+# Create tables in the database
+with app.app_context():
+    db.create_all()
