127c6c8ceb954d3ff761fabb103b5e8b256c99a8
BlairMutisya <tonnyblair09@gmail.com>
2024-08-11 01:36:47 +0300

    Initialize Flask app with core configurations

--- back-end/run.py
+++ back-end/run.py
@@ -14,3 +14,18 @@ from werkzeug.utils import secure_filename
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager, create_access_token, verify_jwt_in_request, get_jwt_identity
 from flask_cors import CORS
+
+# Initialize the Flask app
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), 'uploads')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'p6608665@gmail.com'
+app.config['MAIL_PASSWORD'] = 'waxg umor dfel ucpo'
+app.config['MAIL_DEFAULT_SENDER'] = 'p6608665@gmail.com'
+app.config['SECRET_KEY'] = secrets.token_hex(16)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['JWT_SECRET_KEY'] = secrets.token_hex(16)
