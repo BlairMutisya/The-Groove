4f2b3e80ed35703808fc710b950aab5e2d6b4cba
BlairMutisya <tonnyblair09@gmail.com>
2024-08-11 01:49:22 +0300

    Add function to confirm email token

--- back-end/run.py
+++ back-end/run.py
@@ -124,3 +124,16 @@ def load_user(user_id):
 def generate_confirmation_token(email):
     serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
     return serializer.dumps(email, salt='email-confirm')
+
+# Function to confirm the token
+def confirm_token(token, expiration=3600):
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    try:
+        email = serializer.loads(
+            token,
+            salt='email-confirm',
+            max_age=expiration
+        )
+    except (SignatureExpired, BadSignature):
+        return False
+    return email
