b6f80e27d5ac70f7ff46a355b0039b6f11fe8649
BlairMutisya <tonnyblair09@gmail.com>
2024-08-11 03:46:56 +0300

    Implement mpesa payment method

--- back-end/App/init.py
+++ back-end/App/init.py
@@ -24,7 +24,7 @@ mail = Mail(app)
 login_manager = LoginManager(app)
 # Initialize the serializer with your app's secret key
 serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
-from Backend.run import admin_routes  # Import routes after app is created
+from backend.run import admin_routes  # Import routes after app is created
 # Create tables in the database
 with app.app_context():
     db.create_all()
